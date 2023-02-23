7b98173e6e2171de2014075ec243ce4ce9e1d20d
shihara <shihara@surge.global>
2023-02-24 01:05:09 +0530

    app entry point updated && dummy view added

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export function App() {
+  return (
+    <>
+      <Text>Hello from SwingZen</Text>
+    </>
+  );
+}
+
+export default App;
