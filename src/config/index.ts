87a42592461f27382d7ca567d51e03fe2b1a9c48
shihara <shihara@surge.global>
2023-02-26 12:59:22 +0530

    tailwind base configurations implemented

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,5 @@
+import { create } from 'twrnc';
+
 export * from './paper.config';
+
+export const tw = create(require('./tailwind.config'));
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
+import { useDeviceContext } from 'twrnc';
 
 import App from './app/App';
+import { tw } from './config';
 import { paperTheme } from './config/paper.config';
 import { initializeStore, store } from './stores/';
 
 initializeStore();
 
 export function Main() {
+  useDeviceContext(tw);
+
   return (
     <Provider store={store}>
       <PaperProvider theme={paperTheme}>
