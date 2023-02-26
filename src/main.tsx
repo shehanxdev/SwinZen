5cfe2bb603ea6c8f97e82388b3aa3aa268926090
shihara <shihara@surge.global>
2023-02-26 12:44:02 +0530

    react native paper base configurations added

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+// TODO::remove relative imports once the TS custom path issue get resolved
 import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 
 import App from './app/App';
+import { paperTheme } from './config/paper.config';
 import { initializeStore, store } from './stores/';
 
 initializeStore();
@@ -9,7 +13,11 @@ initializeStore();
 export function Main() {
   return (
     <Provider store={store}>
-      <App />
+      <PaperProvider theme={paperTheme}>
+        <SafeAreaProvider>
+          <App />
+        </SafeAreaProvider>
+      </PaperProvider>
     </Provider>
   );
 }
