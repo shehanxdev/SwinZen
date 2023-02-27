8b360a49bd1e86edc8d6ddac3b129d9866102e00
shihara <shihara@surge.global>
2023-02-27 20:14:18 +0530

    feat: type alias babel and tsconfig changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-import { tw } from './../config';
+import { tw } from '@config/index';
 
 export function App() {
   return (
