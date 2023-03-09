ae4be6a40bb44960fc03272d8e4f4b07e4f92545
shihara <shihara@surge.global>
2023-03-09 18:58:44 +0530

    chore: minor typo change in eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: ['airbnb-typescript', 'prettier'],
   plugins: ['prettier', 'import'],
   rules: {
-    'prettier/prettier': ['off'], //NOTE::Turning on this option will provide sync between eslint and prettier plugins. But it can cause some inconsistence sugestions. So currently it's being turned off.
+    'prettier/prettier': ['off'], //NOTE::Turning on this option since it only will provides sync between eslint and prettier plugins. But it can cause some inconsistence sugestions.
     'react/jsx-filename-extension': 'off',
     '@typescript-eslint/no-shadow': 'off',
   },
