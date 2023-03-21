7c5f2c3ef68bef629355ef7673600cff479b3419
shihara <shihara@surge.global>
2023-03-21 23:17:46 +0530

    feat(OTPInput): OTPInput component implementations

---
+++ src/screens/auth/components/OTPInput/OTPInput.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { CodeField, Cursor, useBlurOnFulfill, useClearByFocusCell } from 'react-native-confirmation-code-field';
+
+import { Text } from '@sz/components';
+import { tw } from '@sz/config';
+import { Color, TextVariant } from '@sz/constants';
+
+const CELL_COUNT = 5;
+
+export interface OTPInputProps {
+  testID?: string;
+  onChangeValue?: (text: string) => void;
+}
+
+export function OTPInput({ testID, onChangeValue }: OTPInputProps) {
+  const [value, setValue] = useState('');
+  /*
+   * This is an additional logic provided by the react-native-confirmation-code-field library
+   * useBlurOnFulfill hook has the logic to blurring <TextInput/> when value all the cells get filled with a value
+   * Visit https://github.com/retyui/react-native-confirmation-code-field/blob/HEAD/API.md#usebluronfulfillvalue-string-cellcount-number-reftextinput
+   * Remove this logic if not needed
+   */
+  const codeFieldRef = useBlurOnFulfill({ value: value, cellCount: CELL_COUNT });
+
+  /*
+   * This is an additional logic provided by the react-native-confirmation-code-field library
+   * useClearByFocusCell hook has the logic clear cells after selecting a middle cell.
+   * ex : 1 2 5 6 7 clicking in 5 will make this 1 2 _ _ _
+   * Visit https://github.com/retyui/react-native-confirmation-code-field/blob/HEAD/API.md#usebluronfulfillvalue-string-cellcount-number-reftextinput
+   * Remove this logic if not needed
+   */
+  const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+    value: value,
+    setValue: setValue,
+  });
+
+  return (
+    <CodeField
+      ref={codeFieldRef}
+      {...props}
+      value={value}
+      onChangeText={value => {
+        setValue(value);
+        onChangeValue(value);
+      }}
+      cellCount={CELL_COUNT}
+      testID={testID}
+      rootStyle={tw`h-18`}
+      keyboardType="number-pad"
+      textContentType="oneTimeCode"
+      renderCell={({ index, symbol, isFocused }) => (
+        <View
+          onLayout={getCellOnLayoutHandler(index)}
+          key={index}
+          style={[
+            tw`h-full rounded-2.5 w-14.5 justify-center items-center`,
+            {
+              backgroundColor: '#283E36', //TODO::update
+            },
+          ]}>
+          <Text variant={TextVariant.SubTitle2SemiBold}>{symbol || (isFocused ? <Cursor /> : null)}</Text>
+          {!symbol && <View style={tw`absolute w-5 h-px bg-[${Color.Neutral.Sz600}] bottom-[17px]`} />}
+        </View>
+      )}
+    />
+  );
+}
