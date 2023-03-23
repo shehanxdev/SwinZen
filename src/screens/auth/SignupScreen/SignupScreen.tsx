2167572763722d708562a8d54ec50d4757590766
Manoj Udeshika <manojudeshika93@gmail.com>
2023-03-23 19:35:34 +0530

    feat: added fields validation

--- src/screens/auth/SignupScreen/SignupScreen.tsx
+++ src/screens/auth/SignupScreen/SignupScreen.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
-import { Image, ScrollView, View } from 'react-native';
+/* eslint-disable import/no-extraneous-dependencies */
+import { yupResolver } from '@hookform/resolvers/yup';
+import _ from 'lodash';
+import React, { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { Image, View } from 'react-native';
+import { KeyboardAvoidingScrollView } from 'react-native-keyboard-avoiding-scroll-view';
 
 import { IMAGES } from '@sz/assets';
 import {
@@ -14,18 +19,32 @@ import {
   TextField,
 } from '@sz/components';
 import { tw } from '@sz/config';
-import { Route, TextVariant } from '@sz/constants';
+import { Color, Route, TextVariant } from '@sz/constants';
 import { NavigationService } from '@sz/services';
+import { schema } from '@sz/utils';
 
 import { GradientBackground } from '../components/GradientBackground';
 
 export function SignupScreen() {
+  const [securePw, setSecurePw] = useState(true);
+  const [secureConfirmPw, setSecureConfirmPw] = useState(true);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: 'onChange', resolver: yupResolver(schema) });
+
+  const onRegister = () => {
+    console.log('User Register Pressed'); // TODO:: integrate APIs into user interface
+  };
+
   return (
-    <GradientBackground>
-      <View style={tw`flex-1 justify-between`} testID="SignupScreenTestID">
+    <GradientBackground testID="SignupScreenTestID">
+      <View style={tw`flex-1 justify-between`}>
         <View style={tw`flex mt-20 mx-5`}>
           <View style={tw`items-center`}>
-            {/*TODO:: Remove this images and replace with SVG later*/}
+            {/*TODO:: remove this images and replace with SVG later*/}
             <Image source={IMAGES.footerLogo} />
           </View>
           <View style={tw`items-center`}>
@@ -34,25 +53,97 @@ export function SignupScreen() {
             </View>
           </View>
         </View>
-        <ScrollView style={tw`flex-1 mx-5`}>
-          <TextField label="Your Name" leftIcon={<ProfileIcon />} />
-          <TextField label="Your Email" leftIcon={<MailIcon />} />
-          <TextField label="Your Password" leftIcon={<AccountLockIcon />} rightIcon={<PasswordRevealIcon />} />
-          <TextField
-            label="Please Confirm Your Password"
-            leftIcon={<AccountLockIcon />}
-            rightIcon={<PasswordRevealIcon />}
+        <KeyboardAvoidingScrollView style={tw`flex-1 mx-5`}>
+          <Controller
+            control={control}
+            name="username"
+            render={({ field: { value, onChange, onBlur } }) => (
+              <TextField
+                label="Your Name"
+                leftIcon={<ProfileIcon />}
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                helperText={errors.username?.message ? errors.username.message.toString() : ''}
+                helperTextColor={Color.Error.SzMain}
+                error={!_.isEmpty(errors.username)}
+              />
+            )}
           />
-          <TextField label="Your Promotion Code" leftIcon={<SecurityIcon />} />
-        </ScrollView>
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { value, onChange, onBlur } }) => (
+              <TextField
+                label="Your Email"
+                leftIcon={<MailIcon />}
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                helperText={errors.email?.message ? errors.email.message.toString() : ''}
+                helperTextColor={Color.Error.SzMain}
+                error={!_.isEmpty(errors.email)}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { value, onChange, onBlur } }) => (
+              <TextField
+                secureTextEntry={securePw}
+                onRightIconPress={() => setSecurePw(!securePw)}
+                label="Your Password"
+                leftIcon={<AccountLockIcon />}
+                rightIcon={<PasswordRevealIcon />}
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                helperText={errors.password?.message ? errors.password.message.toString() : ''}
+                helperTextColor={Color.Error.SzMain}
+                error={!_.isEmpty(errors.password)}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="confirmPassword"
+            render={({ field: { value, onChange, onBlur } }) => (
+              <TextField
+                secureTextEntry={secureConfirmPw}
+                onRightIconPress={() => setSecureConfirmPw(!secureConfirmPw)}
+                label="Please Confirm Your Password"
+                leftIcon={<AccountLockIcon />}
+                rightIcon={<PasswordRevealIcon />}
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                helperText={errors.confirmPassword?.message ? errors.confirmPassword.message.toString() : ''}
+                helperTextColor={Color.Error.SzMain}
+                error={!_.isEmpty(errors.confirmPassword)}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="promoCode"
+            render={({ field: { value, onChange, onBlur } }) => (
+              <TextField
+                label="Your Promotion Code"
+                leftIcon={<SecurityIcon />}
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                helperText={errors.promoCode?.message ? errors.promoCode.message.toString() : ''}
+                helperTextColor={Color.Error.SzMain}
+                error={!_.isEmpty(errors.promoCode)}
+              />
+            )}
+          />
+        </KeyboardAvoidingScrollView>
         <View style={tw`items-center mt-10 mb-5 mx-5`}>
           <View style={tw`mb-2`}>
-            <Button
-              onPress={() => {
-                //TODO:: Implement with the API integration
-              }}
-              title={'Register'}
-            />
+            <Button onPress={() => handleSubmit(onRegister)} title={'Register'} />
           </View>
           <Text variant={TextVariant.Body2Regular}>
             Already have an account?
