d91afcdf419a55968f4cdd501a2ca2c79b4a9567
Manoj Udeshika <manojudeshika93@gmail.com>
2023-03-27 10:46:43 +0530

    chore: fixed typos

--- src/utils/validations/auth/resetPassword.validations.ts
+++ src/utils/validations/auth/resetPassword.validations.ts
@@ -6,16 +6,16 @@ export const resetPasswordValidationSchema = yup
   .object({
     password: yup
       .string()
-      .required('Please enter Password')
+      .required('Please enter New password')
       .min(8, 'Password must be at least 8 letters long')
       .max(20, 'Password must not be 20 letters long')
       .matches(
         ValidPasswordRegex,
-        'Password must contain at least one Uppercase letter, Lowercase letter, Numeric character and Special character',
+        'Your password does not meet our password rules, Password must contain at least one Uppercase letter, Lowercase letter, Numeric character and Special character',
       ),
     confirmPassword: yup
       .string()
-      .required('Please confirm Password')
+      .required('Please enter Password confirmation')
       .oneOf([yup.ref('password')], 'Your passwords do not match'),
   })
   .required();
