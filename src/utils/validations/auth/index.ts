08646503d376bce2a28c7a1dd930c97f0d8d652f
Manoj Udeshika <manojudeshika93@gmail.com>
2023-03-27 13:44:09 +0530

    chore: sorted exports

--- src/utils/validations/auth/index.ts
+++ src/utils/validations/auth/index.ts
@@ -1,4 +1,4 @@
 export * from './forgotPassword.validations';
 export * from './login.validations';
-export * from './signup.validations';
 export * from './resetPassword.validations';
+export * from './signup.validations';
