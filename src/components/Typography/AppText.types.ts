4a8e91d4e6f7d94a9809af3bb457674f368bebd4
Manoj Udeshika <manojudeshika93@gmail.com>
2023-03-28 21:09:15 +0530

    chore: removed duplicate prop

--- src/components/Typography/AppText.types.ts
+++ src/components/Typography/AppText.types.ts
@@ -4,7 +4,7 @@ import { Color, TextAlignment, TextVariant } from '@sz/constants';
 
 type RNTextProps = React.ComponentProps<typeof Text>;
 
-export type WithRNTextProps = Pick<RNTextProps, 'onPress' | 'testID' | 'numberOfLines'>;
+export type WithRNTextProps = Pick<RNTextProps, 'testID' | 'numberOfLines'>;
 
 export interface AppTextProps extends WithRNTextProps {
   color?: Color;
