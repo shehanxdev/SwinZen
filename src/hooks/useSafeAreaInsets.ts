72b34704eec4b99748cfa43409e04f71f590021d
shihara <shihara@surge.global>
2023-03-29 17:22:40 +0530

    chore: refactored useSafeAreaInsets custom hook && added unit tests

--- src/hooks/useSafeAreaInsets.ts
+++ src/hooks/useSafeAreaInsets.ts
@@ -5,10 +5,12 @@ import { EdgeInsets, SafeAreaInsetsContext } from 'react-native-safe-area-contex
  * This hook is a drop in replacement for `react-native-safe-area-context/useSafeAreaInsets` that won't
  * throw an error if the hook is called outside of a `SafeAreaProvider`
  * This is required since we have some issues with wrapping SafeAreaProvider inside renderWithProviders for JEST testing
- * In addtion to this we can pass an option to include custom the header & bottom bar heights in the returned insets.
+ * In addtion to this we can refactor this to pass an option to include custom the header & bottom bar heights in the returned insets.
  */
+const defaultInsets = { top: 0, right: 0, bottom: 0, left: 0 };
+
 export function useSafeAreaInsets(): EdgeInsets {
-  const insets = { ...useContext(SafeAreaInsetsContext) } ?? { top: 0, right: 0, bottom: 0, left: 0 };
+  const insets = { ...(useContext(SafeAreaInsetsContext) ?? defaultInsets) };
 
   return insets;
 }
