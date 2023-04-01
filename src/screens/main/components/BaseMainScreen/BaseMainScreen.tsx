d8f77cf5bf9cc9f628201b872d58b8b2e76b5dce
shihara <shihara@surge.global>
2023-04-01 15:32:22 +0530

    chore: removed redundant images

--- src/screens/main/components/BaseMainScreen/BaseMainScreen.tsx
+++ src/screens/main/components/BaseMainScreen/BaseMainScreen.tsx
@@ -1,6 +1,7 @@
 import React, { PropsWithChildren } from 'react';
 import { ImageBackground, SafeAreaView, ScrollView } from 'react-native';
 
+import { images } from '@sz/assets';
 import { tw } from '@sz/config';
 
 interface BaseMainScreenProps extends PropsWithChildren {
@@ -9,8 +10,7 @@ interface BaseMainScreenProps extends PropsWithChildren {
 
 export function BaseMainScreen({ children, testID }: BaseMainScreenProps) {
   return (
-    // TODO::This base screen has a tempory image background inorder to demonstrate the blured bottom tab bar. Replace with the correct backgroudn image when developing these screens
-    <ImageBackground testID={testID} style={tw`flex-1`} source={require('./tempory-gradiant-background.png')}>
+    <ImageBackground testID={testID} style={tw`flex-1`} source={images.gradientBackground}>
       <SafeAreaView>
         <ScrollView>{children}</ScrollView>
       </SafeAreaView>
