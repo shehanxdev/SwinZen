b783e995be0cf30575a8958175eeeb2258aff3fb
shihara <shihara@surge.global>
2023-04-04 15:28:17 +0530

    refactor(http-service-unit-tests): refactored http unit tests

--- src/services/http-service/__tests__/HttpService.spec.ts
+++ src/services/http-service/__tests__/HttpService.spec.ts
@@ -1,29 +1,37 @@
 import { ApisauceInstance } from 'apisauce';
 import MockAdapter from 'axios-mock-adapter';
 
-import { HttpService } from '../HttpService';
+import { GetAccessTokenCallback, GetRefreshTokenCallback, HttpService, OnTokenUpdateHandler } from '../HttpService';
+
+const INITIAL_TOKENS = ['INITIAL_AUTH', 'INITIAL_REFRESH'];
 
 const baseUrl = 'http://localhost:3000';
+
+const noop = () => null;
 
 interface HttpServiceWithPrivates extends Omit<HttpService, 'apiSauce' | 'apiSauceWithoutAuth'> {
   apiSauce: ApisauceInstance;
   apiSauceWithoutAuth: ApisauceInstance;
 }
 
+const getHttpService = ({
+  getAccessToken,
+  getRefreshToken,
+  onTokenUpdate,
+}: Partial<{
+  getAccessToken: GetAccessTokenCallback;
+  getRefreshToken: GetRefreshTokenCallback;
+  onTokenUpdate: OnTokenUpdateHandler;
+}> = {}) => {
+  return new HttpService(
+    baseUrl,
+    getAccessToken || (() => INITIAL_TOKENS[0]),
+    getRefreshToken || (() => INITIAL_TOKENS[1]),
+    onTokenUpdate || noop,
+  ) as unknown as HttpServiceWithPrivates;
+};
+
 describe('HTTP Service', () => {
-  const mockGetAccessTokenCallBack = jest.fn();
-  const mockGetRefreshTokenCallBack = jest.fn();
-  const mockOnTokenUpdateCallBack = jest.fn();
-
-  const getHttpService = () => {
-    return new HttpService(
-      baseUrl,
-      mockGetAccessTokenCallBack,
-      mockGetRefreshTokenCallBack,
-      mockOnTokenUpdateCallBack,
-    ) as unknown as HttpServiceWithPrivates;
-  };
-
   it('should initialize when provided with correct params', () => {
     const httpService = getHttpService();
     expect(httpService).toBeInstanceOf(HttpService);
@@ -43,5 +51,36 @@ describe('HTTP Service', () => {
     await httpService.getAnonymous('/sample');
   });
 
-  //TODO::add other unit tests once the APIs are ready
+  it('should automatically add JWT auth token to authenticated requests', async () => {
+    const httpService = getHttpService();
+
+    const mock = new MockAdapter(httpService.apiSauce.axiosInstance, {
+      delayResponse: 100,
+    });
+
+    mock.onAny().reply(config => {
+      const authorizationHeader = config.headers.Authorization;
+      expect(authorizationHeader).toBe(`Bearer ${INITIAL_TOKENS[0]}`);
+      return [200];
+    });
+
+    await httpService.get('/sample');
+  });
+
+  it('should prepend base url to requests', async () => {
+    const httpService = getHttpService();
+
+    const mock = new MockAdapter(httpService.apiSauce.axiosInstance, {
+      delayResponse: 100,
+    });
+
+    mock.onAny().reply(config => {
+      expect(config.baseURL).toBe(baseUrl);
+      return [200];
+    });
+
+    await httpService.get('/sample');
+  });
+
+  //TODO::Add unit tests related to token expired scenario after the refresh token logic get implemented
 });
