0f60e7bdd54924284e6bfa27626357ac9fd7595b
Manoj Udeshika <manojudeshika93@gmail.com>
2023-04-05 20:53:15 +0530

    chore: improved masking function

--- src/utils/helpers/helpers.ts
+++ src/utils/helpers/helpers.ts
@@ -1,7 +1,7 @@
-export function getMaskedMail(email: string) {
-  // Note: This use to make masked mail with the first three letters and the mail domain
-  const splittedMail = email?.split('@');
-  const maskedMail =
-    splittedMail && splittedMail[0].substring(0, 3) + '*'.repeat(splittedMail[0].length - 3) + '@' + splittedMail[1];
-  return maskedMail;
+// Note: This use to make masked mail with the first and last letter and the mail domain
+export function getMaskedMail(email: string, mask?: string) {
+  mask = mask || '*';
+  return email.replace(/^(.)(.*)(.@.*)$/, function (_, a, b, c) {
+    return a + b.replace(/./g, mask) + c;
+  });
 }
