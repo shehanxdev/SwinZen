d5799a3b026d98d78f5a99eb83e3a4ea5779030f
Manoj Udeshika <manojudeshika93@gmail.com>
2023-04-06 12:10:25 +0530

    feat: added unit tests for the mail masking function

--- src/utils/helpers/helpers.ts
+++ src/utils/helpers/helpers.ts
@@ -1,4 +1,11 @@
-// Note: This use to make masked mail with the first and last letter and the mail domain
+/**
+ * Returns a masked version of an email address, replacing the characters between
+ * the first character and the character before "@" symbol with a mask character (defaults to "*").
+ *
+ * @param {string} email - The email address to mask.
+ * @param {string=} mask - The character to use as the mask (default "*").
+ * @returns {string} The masked email address.
+ */
 export function getMaskedMail(email: string, mask?: string) {
   mask = mask || '*';
   return email.replace(/^(.)(.*)(.@.*)$/, function (_, a, b, c) {
