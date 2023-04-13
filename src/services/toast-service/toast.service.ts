92793b4876b8197bab923ddd3ffbf269ac4c6911
shihara <shihara@surge.global>
2023-04-13 21:19:36 +0530

    chore: minor change in toast service

--- src/services/toast-service/toast.service.ts
+++ src/services/toast-service/toast.service.ts
@@ -5,7 +5,7 @@ import { Color } from '@sz/constants';
 type ToastParams = {
   message: string;
   description: string;
-  otherConfigs?: Partial<MessageOptions>;
+  otherConfigs?: Omit<MessageOptions, 'message' | 'description'>;
 };
 
 /*
