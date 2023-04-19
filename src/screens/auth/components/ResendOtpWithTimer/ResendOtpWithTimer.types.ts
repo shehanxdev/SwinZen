12dd3f67bcb0f9ff2f67c28154d2abb7524069e7
shihara <shihara@surge.global>
2023-04-20 01:29:44 +0530

    chore: minor typo fix in resendOtpWithTimer component

--- src/screens/auth/components/ResendOtpWithTimer/ResendOtpWithTimer.types.ts
+++ src/screens/auth/components/ResendOtpWithTimer/ResendOtpWithTimer.types.ts
@@ -7,7 +7,7 @@ type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumer
 //Only the values between 0 and 11 can be used for the minutes in OTP timer. Decimal numbers like 1.5 cannot be used. Values greater than 10 or below than 0 also cannot be used.
 export type OtpMinutes = IntRange<0, 11>;
 
-//Only the values between 0 and 60 can be used for the minutes in OTP timer. Decimal numbers like 55.5 cannot be used. Values greater than 59 or below than 0 also cannot be used.
+//Only the values between 0 and 60 can be used for the seconds in OTP timer. Decimal numbers like 55.5 cannot be used. Values greater than 59 or below than 0 also cannot be used.
 export type OtpSeconds = IntRange<0, 60>;
 
 export interface ResendOtpWithTimerProps {
