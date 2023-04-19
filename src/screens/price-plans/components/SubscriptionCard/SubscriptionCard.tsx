08f13cd7be805aa91e9dbb9e4468007e74989ae8
Shehan Chanuka <shehan.chanuka@surge.global>
2023-04-19 12:19:03 +0530

    feat: added border colors and button color changes when card is selected

--- src/screens/price-plans/components/SubscriptionCard/SubscriptionCard.tsx
+++ src/screens/price-plans/components/SubscriptionCard/SubscriptionCard.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import { LinearGradientProps } from 'react-native-linear-gradient';
 import { Card } from 'react-native-paper';
 import tw from 'twrnc';
 
@@ -10,9 +9,8 @@ import { Button } from '@sz/components';
 import { Text } from '@sz/components';
 import { Color, TextAlignment, TextVariant } from '@sz/constants';
 
-export interface PriceCardProps {
-  testID: string;
-  LinearGradientProps?: Partial<LinearGradientProps>;
+export interface SubscriptionCardProps {
+  testID?: string;
   title: string;
   subTitle: string;
   loading?: boolean;
@@ -20,6 +18,9 @@ export interface PriceCardProps {
   pricingDescription: string;
   featureList: string[];
   buttonColor?: Color;
+  onCardPress?: () => void;
+  onGetStarted?: () => void;
+  isSelected?: boolean;
 }
 
 export function SubscriptionCard({
@@ -28,51 +29,61 @@ export function SubscriptionCard({
   subTitle,
   price,
   pricingDescription,
-  testID = 'RNCardSubscriptionCard',
-  LinearGradientProps = { colors: ['#8EFF001F', '#95FF101D', '#FFFFFF1F'], locations: [0, 0.0719, 1] }, //TODO :: these colors are not in the design system
-}: PriceCardProps) {
+  testID = 'SubscriptionCard',
+  onCardPress,
+  onGetStarted,
+  isSelected,
+}: SubscriptionCardProps) {
   return (
-    <View style={tw`mx-12`}>
-      <Card testID={testID} style={tw`bg-transparent shadow-none`}>
-        <LinearGradient
-          colors={LinearGradientProps.colors}
-          locations={LinearGradientProps.locations}
-          style={tw`flex-col items-center px-8 py-4 rounded-xl`}>
-          <View style={tw`mt-4`}>
-            <Text variant={TextVariant.SubTitle1} color={Color.Primary.Sz200}>
-              {title}
-            </Text>
-            <Text variant={TextVariant.Labels} color={Color.Neutral.Sz100}>
-              {subTitle}
-            </Text>
-          </View>
-          <View style={tw`mt-8`}>
-            <Text variant={TextVariant.Heading2} color={Color.Primary.Sz400}>
-              {`$${price}`}
-            </Text>
-            <Text variant={TextVariant.Body2Regular} color={Color.Neutral.Sz100}>
-              {pricingDescription}
-            </Text>
-          </View>
-          <View style={tw`mt-8`}>
-            {featureList.map((feature, index) => {
-              return (
-                <View style={tw`flex-row items-start gap-2`} key={index}>
-                  <View style={tw`mt-2`}>
-                    <PricePlanFeatureListTickIcon width={12} height={12} />
-                  </View>
-                  <Text textAlign={TextAlignment.Left} variant={TextVariant.Labels} color={Color.Neutral.Sz100}>
-                    {feature}
-                  </Text>
+    <Card
+      onPress={onCardPress}
+      testID={testID}
+      style={tw`bg-[${Color.Transparency.full}] ${isSelected ? `border-[${Color.Neutral.Sz500}]` : ''} ${
+        isSelected ? `border` : ''
+      } shadow-none`}>
+      <LinearGradient
+        colors={['#8EFF001F', '#95FF101D', '#FFFFFF1F']} //TODO :: these colors are not in the design system
+        locations={[0, 0.0719, 1]}
+        style={tw`flex-col items-center px-8 py-4 rounded-xl`}>
+        <View style={tw`mt-4`}>
+          <Text variant={TextVariant.SubTitle1} color={Color.Primary.Sz200}>
+            {title}
+          </Text>
+          <Text variant={TextVariant.Labels} color={Color.Neutral.Sz100}>
+            {subTitle}
+          </Text>
+        </View>
+        <View style={tw`mt-8`}>
+          <Text variant={TextVariant.Heading2} color={Color.Primary.Sz400}>
+            {`$${price}`}
+          </Text>
+          <Text variant={TextVariant.Body2Regular} color={Color.Neutral.Sz100}>
+            {pricingDescription}
+          </Text>
+        </View>
+        <View style={tw`mt-8`}>
+          {featureList.map((feature, index) => {
+            return (
+              <View style={tw`flex-row items-start gap-2`} key={index}>
+                <View style={tw`mt-2`}>
+                  <PricePlanFeatureListTickIcon width={12} height={12} />
                 </View>
-              );
-            })}
-          </View>
-          <View style={tw`mt-8 w-50`}>
-            <Button title="Get Started" fullWidth={true} />
-          </View>
-        </LinearGradient>
-      </Card>
-    </View>
+                <Text textAlign={TextAlignment.Left} variant={TextVariant.Labels} color={Color.Neutral.Sz100}>
+                  {feature}
+                </Text>
+              </View>
+            );
+          })}
+        </View>
+        <View style={tw`mt-8 w-50`}>
+          <Button
+            backgroundColor={isSelected ? Color.Neutral.Sz500 : Color.Primary.Sz600}
+            onPress={onGetStarted}
+            title="Get Started"
+            fullWidth={true}
+          />
+        </View>
+      </LinearGradient>
+    </Card>
   );
 }
