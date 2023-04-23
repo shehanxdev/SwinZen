ab0daf2e48449f97bb368aabb40194e4ce2dfc75
shihara <shihara@surge.global>
2023-04-24 02:21:25 +0530

    chore: minor props changes in MobileNumberField component

--- src/components/Input/MobileNumberInput/MobileNumberInput.tsx
+++ src/components/Input/MobileNumberInput/MobileNumberInput.tsx
@@ -5,10 +5,14 @@ import { PhoneIcon } from '../../Icon';
 import { MaskedInput } from '../MaskedInput';
 import { TextFieldProps } from './../../TextField';
 
+type MobileNumberFieldProps = Omit<TextFieldProps, 'leftIcon' | 'rightIcon' | 'keyboardType' | 'onRightIconPress'>;
+
 //TODO::update the mask depending on the requirements, handle leading zero and country once the requirements get finalized
-export const MobileNumberField = forwardRef<RNTextInput, Omit<TextFieldProps, 'leftIcon'>>(function MobileNumberInput(
+export const MobileNumberField = forwardRef<RNTextInput, MobileNumberFieldProps>(function MobileNumberInput(
   { ...props },
   ref,
 ) {
-  return <MaskedInput inputMask={'+1 ### ### ####'} ref={ref} leftIcon={<PhoneIcon />} {...props} />;
+  return (
+    <MaskedInput inputMask={'+1 ### ### ####'} ref={ref} leftIcon={<PhoneIcon />} {...props} keyboardType="phone-pad" />
+  );
 });
