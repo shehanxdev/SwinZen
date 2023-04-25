f0a0a7b64b63e2fd929a9158f4fcd258239a48b8
shihara <shihara@surge.global>
2023-04-26 00:27:25 +0530

    chore: minor import update in ContactUsScreen

--- src/screens/info/ContactUsScreen/ContactUsScreen.tsx
+++ src/screens/info/ContactUsScreen/ContactUsScreen.tsx
@@ -5,15 +5,15 @@ import { Link } from '@sz/components';
 import { tw } from '@sz/config';
 import { NavigationService } from '@sz/services';
 
-import { BaseScreen } from './../../../screens/account/components';
+import { BaseInfoScreen } from '../components';
 
 export function ContactUsScreen() {
   return (
-    <BaseScreen>
+    <BaseInfoScreen>
       <View style={tw`m-auto`}>
         <Text style={tw`m-10`}>Contact Us Screen</Text>
         <Link text="Go Back" onPress={() => NavigationService.goBack()} />
       </View>
-    </BaseScreen>
+    </BaseInfoScreen>
   );
 }
