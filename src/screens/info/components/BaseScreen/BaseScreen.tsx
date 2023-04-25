463c3efd65a7c8656e696cd2fd249390329df483
Manoj Udeshika <manojudeshika93@gmail.com>
2023-04-25 07:12:05 +0530

    chore: fixed screen header issue

--- src/screens/info/components/BaseScreen/BaseScreen.tsx
+++ src/screens/info/components/BaseScreen/BaseScreen.tsx
@@ -6,6 +6,7 @@ import { LinearGradientProps } from 'react-native-linear-gradient';
 import { images } from '@sz/assets';
 import { tw } from '@sz/config';
 import { Color } from '@sz/constants';
+import { useHeaderHeight } from '@sz/hooks';
 
 interface BaseScreenProps extends Partial<LinearGradientProps> {
   children: React.ReactNode;
@@ -16,10 +17,12 @@ export function BaseScreen({
   colors = ['#1A5C23', Color.Primary.Sz800, Color.Primary.Sz900], //TODO:: update, these colours are NOT available within the design system
   ...otherlinearGradientProps
 }: BaseScreenProps) {
+  const headerHeight = useHeaderHeight();
+
   return (
     <LinearGradient {...otherlinearGradientProps} colors={colors} style={tw`relative flex-1`}>
       <Image source={images.grassBackground} style={tw`flex-1 opacity-10`} resizeMode="repeat" />
-      <SafeAreaView style={tw`absolute h-full`}>
+      <SafeAreaView style={tw`absolute h-full pt-[${headerHeight}px]`}>
         <KeyboardAvoidingView style={tw`flex-1`} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
           <ScrollView contentContainerStyle={tw`grow`} keyboardShouldPersistTaps="handled">
             {children}
