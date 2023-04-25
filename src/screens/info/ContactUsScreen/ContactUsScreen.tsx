232b0354dc9551c3f082b9f6e81bd4172bff56f1
Shehan Chanuka <shehan.chanuka@surge.global>
2023-04-25 11:34:33 +0530

    feat: contact us screen is being developed

--- src/screens/info/ContactUsScreen/ContactUsScreen.tsx
+++ src/screens/info/ContactUsScreen/ContactUsScreen.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
-import { Link } from '@sz/components';
+import { Button, Text } from '@sz/components';
 import { tw } from '@sz/config';
-import { NavigationService } from '@sz/services';
+import { TextVariant } from '@sz/constants';
 
 import { BaseScreen } from '../components';
 
 export function ContactUsScreen() {
   return (
-    <BaseScreen>
-      <View style={tw`m-auto`}>
-        <Text style={tw`m-10`}>Contact Us Screen</Text>
-        <Link text="Go Back" onPress={() => NavigationService.goBack()} />
+    <BaseScreen testID="ContactUsScreenTestID">
+      <Text variant={TextVariant.Body1Regular}>
+        We'd Love to Hear From You. Your feedback is what drives us to build a better service for you. Please contact us
+        through the following form
+      </Text>
+      <View style={tw`flex-1 justify-between `}>
+        <View style={tw`flex-1 mx-5`}>{/* todo: */}</View>
+        <View style={tw`items-center mt-18 mb-5 mx-5`}>
+          <View style={tw`mb-6`}>
+            <Button
+              // onPress={()=>{todo:implement}}
+              title={'SUBMIT'}
+              //loading={loading}
+            />
+          </View>
+        </View>
       </View>
     </BaseScreen>
   );
