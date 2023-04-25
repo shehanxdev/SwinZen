298e463a9046745dd479b14f4150448a90b93465
Madusha Lakruwan <madusha@surge.global>
2023-04-25 13:52:01 +0530

    refactor: refactor

--- src/screens/info/FAQScreen/components/FAQSectionContent/FAQSectionContent.tsx
+++ src/screens/info/FAQScreen/components/FAQSectionContent/FAQSectionContent.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 import { Text } from '@sz/components';
 import { Color, TextAlignment, TextVariant } from '@sz/constants';
+import { FaqSection } from '@sz/models';
 
-export function FAQSectionContent(section) {
+export function FAQSectionContent(section: FaqSection) {
   return (
     <Text variant={TextVariant.Body2Regular} color={Color.Neutral.Sz500} textAlign={TextAlignment.Left}>
       {section.content}
--- src/screens/info/components/index.ts
+++ src/screens/info/components/index.ts
@@ -1,3 +1,1 @@
 export * from './BaseScreen';
-export * from './FAQSectionHeader';
-export * from './FAQSectionContent';
