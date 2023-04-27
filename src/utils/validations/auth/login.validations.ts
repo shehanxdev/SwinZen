2182694aa5ca45b411d55277229082fce7e4ab19
Manoj Udeshika <manojudeshika93@gmail.com>
2023-04-27 16:05:29 +0530

    chore: fixed validation issue

--- src/utils/validations/auth/login.validations.ts
+++ src/utils/validations/auth/login.validations.ts
@@ -1,33 +1,26 @@
 import * as yup from 'yup';
 
-import { ValidPasswordRegex } from '@sz/constants';
-
 export const loginErrorMessages = {
   'username:required': 'Please enter Email ID',
   'username:invalid': 'Invalid Email',
   'username:max': 'Email must not be 50 letters long',
+
   'password:required': 'Please enter Password',
-  'password:min': 'Password must be at least 8 letters long',
-  'password:max': 'Password must not be 20 letters long',
-  'password:match':
-    'Password must contain at least one Uppercase letter, Lowercase letter, Numeric character and Special character',
+  'password:min': 'Password must be between 8 and 20 characters length',
+  'password:max': 'Password must be between 8 and 20 characters length',
 };
 
 export const loginValidationSchema = yup
   .object({
     username: yup
       .string()
-      .required('Please enter Email ID')
-      .email('Invalid Email')
-      .max(50, 'Email must not be 50 letters long'),
+      .required(loginErrorMessages['username:required'])
+      .email(loginErrorMessages['username:invalid'])
+      .max(50, loginErrorMessages['username:max']),
     password: yup
       .string()
-      .required('Please enter Password')
-      .min(8, 'Password must be at least 8 letters long')
-      .max(20, 'Password must not be 20 letters long')
-      .matches(
-        ValidPasswordRegex,
-        'Password must contain at least one Uppercase letter, Lowercase letter, Numeric character and Special character',
-      ),
+      .required(loginErrorMessages['password:required'])
+      .min(8, loginErrorMessages['password:min'])
+      .max(20, loginErrorMessages['password:max']),
   })
   .required();
