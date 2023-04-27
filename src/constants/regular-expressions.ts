641b1f0c5e2e568580df3a93c4fa99e4cbf27b87
Shehan Chanuka <shehan.chanuka@surge.global>
2023-04-28 04:08:41 +0530

    chore: synced with dev

--- src/constants/regular-expressions.ts
+++ src/constants/regular-expressions.ts
@@ -1,3 +1,2 @@
-export const ValidPasswordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/;
 export const ValidOtpRegex = /^[0-9]{6}$/;
 export const ValidMobileNumberRegex = /^\+?1?\s?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[0-9]{3}[\s\-]?[0-9]{4}$/; //todo need to change the regex accordingly
