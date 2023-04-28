425d485e99a47821d86520f720efbc7c363535ce
Manoj Udeshika <manojudeshika93@gmail.com>
2023-04-28 11:59:34 +0530

    chore: changed the card color into updated design

--- src/screens/price-plans/components/SubscriptionCard/SubscriptionCard.tsx
+++ src/screens/price-plans/components/SubscriptionCard/SubscriptionCard.tsx
@@ -32,7 +32,7 @@ export function SubscriptionCard({
     <TouchableOpacity testID={testID} onPress={onCardPress}>
       <Card style={tw`bg-[${Color.Primary.Sz700}]/50 rounded-2.5 content-center`}>
         {betterValue && (
-          <View style={tw`bg-[${Color.Tertiary.Sz900}] h-6.25 rounded-t-2.5`}>
+          <View style={tw`bg-[${selected ? Color.Neutral.Sz600 : Color.Tertiary.Sz900}] h-6.25 rounded-t-2.5`}>
             <Text variant={TextVariant.Labels} color={Color.Primary.Sz900}>
               {betterValue}
             </Text>
