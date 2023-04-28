ee2fe35c0a090e01ddedd28d58bc413b5ef6ce19
Manoj Udeshika <manojudeshika93@gmail.com>
2023-04-28 15:14:05 +0530

    chore: modified function comments

--- src/utils/helpers/moment.ts
+++ src/utils/helpers/moment.ts
@@ -2,10 +2,9 @@
 import moment from 'moment';
 
 /**
- * Returns a masked version of an email address, replacing the characters between
- * the first character and the character before "@" symbol with a mask character (defaults to "*").
+ * Returns a moment object that with modificatons
  *
- * @param {string | Date} data - The email address to mask.
+ * @param {string | Date} data - The data/time string or Date object.
  */
 export function customMoment(data?: string | Date) {
   // manipulating moment calender according to the design, should config when component is rendering and testing
