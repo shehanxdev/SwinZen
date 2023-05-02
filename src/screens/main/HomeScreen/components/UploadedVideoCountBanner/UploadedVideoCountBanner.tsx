bdf6464f258db68f68f4974db77bed42c97f6db0
shihara <shihara@surge.global>
2023-05-02 06:56:07 +0530

    feat: uploadedVideoCountBanner implementations

---
+++ src/screens/main/HomeScreen/components/UploadedVideoCountBanner/UploadedVideoCountBanner.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { tw } from '@sz/config';
+import { Color } from '@sz/constants';
+
+import { VideoCountCard } from './components';
+
+interface UploadedVideoCountBannerProps {
+  VideoCount: number | string;
+  SwinZenUniVideoCount: number | string;
+}
+
+export function UploadedVideoCountBanner({ VideoCount = 0, SwinZenUniVideoCount = 0 }: UploadedVideoCountBannerProps) {
+  return (
+    <View style={[tw`flex flex-row justify-between min-h-15`]}>
+      <VideoCountCard count={VideoCount} description={`Video\nUploads`} />
+      <View style={tw`w-2.5`} />
+      <VideoCountCard
+        count={SwinZenUniVideoCount}
+        description={`SwingZen\nUniversity`}
+        backgroundColor={Color.Tertiary.Sz900}
+        countTextColor={Color.Neutral.Sz1000}
+        descriptionTextColor={Color.Neutral.Sz1000}
+      />
+    </View>
+  );
+}
---
+++ src/screens/main/HomeScreen/components/UploadedVideoCountBanner/components/VideoCountCard/VideoCountCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Text } from '@sz/components';
+import { tw } from '@sz/config';
+import { Color, TextAlignment, TextVariant } from '@sz/constants';
+
+interface VideoCountCardProps {
+  teseID?: string;
+  count: number | string;
+  description: string;
+  backgroundColor?: Color;
+  countTextColor?: Color;
+  descriptionTextColor?: Color;
+}
+
+export function VideoCountCard({
+  teseID,
+  count,
+  description,
+  backgroundColor = '#1A5C23' as Color,
+  countTextColor = Color.Neutral.White,
+  descriptionTextColor = Color.Neutral.White,
+}: VideoCountCardProps) {
+  return (
+    <View testID={teseID} style={tw`flex-row justify-start items-center flex-1 rounded-2.5 bg-[${backgroundColor}]`}>
+      <View style={tw`mr-4 ml-2.5`}>
+        <Text variant={TextVariant.Heading2} color={countTextColor}>
+          {count}
+        </Text>
+      </View>
+      <Text variant={TextVariant.Body2Regular} textAlign={TextAlignment.Left} color={descriptionTextColor}>
+        {description}
+      </Text>
+    </View>
+  );
+}
+
+//Video {'\n'}Uploads
