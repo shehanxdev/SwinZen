077a90dce745ccc2175d40f0e7803ab3ef5a9a8c
shihara <shihara@surge.global>
2023-05-10 21:14:52 +0530

    chore: removed unnecessary date time formats

--- src/constants/data-time.ts
+++ src/constants/data-time.ts
@@ -5,10 +5,7 @@ export const dateFormats = {
   scratchedDateAndTime: 'HH:MM, DD/MM/YY',
   singleDatePicker: 'ddd, MMM DD',
   textInput: 'DD / MM / YY',
-  extrasMarketHeader: 'dd/MM/YY',
-  finalResultsHeader: 'dd/MM/YYYY',
   dayAndDate: 'ddd, DD',
   timestamp: 'DD/MM/YYYY HH:mm:ssPM Z',
-  advertisedStartTime: 'ddd, DD MMM YYYY HH:mm PM',
   saveFileTimestamp: 'DD_MM_YYYY_HH_mm_ss',
 };
