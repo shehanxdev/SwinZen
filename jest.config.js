360389da5dd462eab25cf6e48c76a9570489bee7
Madusha Lakruwan <madusha@surge.global>
2023-05-18 18:59:28 +0530

    chore: update pipeline to run unit tests and pass coverage to sonarcloud

--- jest.config.js
+++ jest.config.js
@@ -28,4 +28,5 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|rollbar-react-native|@fortawesome|@react-native|@react-navigation|@react-native-firebase/messaging)',
   ],
+  coverageDirectory: 'coverage',
 };
