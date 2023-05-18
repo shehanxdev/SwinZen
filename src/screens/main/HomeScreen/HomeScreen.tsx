b2b24c5b23cf2f446baded023d4149d413a2368e
Manoj Udeshika <manojudeshika93@gmail.com>
2023-05-18 08:30:36 +0530

    chore: added firebase configs

--- src/screens/main/HomeScreen/HomeScreen.tsx
+++ src/screens/main/HomeScreen/HomeScreen.tsx
@@ -1,3 +1,4 @@
+import messaging from '@react-native-firebase/messaging';
 import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
 
@@ -7,9 +8,17 @@ import { PermissionService } from '@sz/services';
 import { BaseMainScreen } from '../components';
 
 export function HomeScreen() {
+  // firebase device registration and store fcm token
+  const registerAppWithFCM = async () => {
+    await messaging().registerDeviceForRemoteMessages();
+    const token = await messaging().getToken();
+    console.log('@@@###', token);
+  };
+
   // To request notifications permissions
   useEffect(() => {
     PermissionService.requestNotificationsPermission();
+    registerAppWithFCM();
   }, []);
 
   return (
