0aec2ca46f35aca953e3899a94a41de3c8a5ceb8
shihara <shihara@surge.global>
2023-05-19 20:43:41 +0530

    feat: Profile image upload component implementation

--- src/screens/account/profile/ProfileSettingsScreen/components/ProfileImageUpload/ProfileImageUpload.tsx
+++ src/screens/account/profile/ProfileSettingsScreen/components/ProfileImageUpload/ProfileImageUpload.tsx
@@ -1,11 +1,20 @@
-import React, { useMemo, useState } from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Alert, TouchableOpacity, View } from 'react-native';
 import FastImage from 'react-native-fast-image';
+import {
+  CameraOptions,
+  ImageLibraryOptions,
+  ImagePickerResponse,
+  launchCamera,
+  launchImageLibrary,
+} from 'react-native-image-picker';
+import { useDispatch } from 'react-redux';
 
 import { ProfileImageChangeCameraIcon, Text } from '@sz/components';
 import { tw } from '@sz/config';
 import { TextVariant } from '@sz/constants';
-import { NavigationService } from '@sz/services';
+import { ToastService } from '@sz/services';
+import { useSelector } from '@sz/stores';
 
 //Note::below values are in PX
 const PROFILE_IMAGE_DIMENTIONS = {
@@ -21,34 +30,88 @@ const CAMERA_ICON_POSITION = {
   top: PROFILE_IMAGE_DIMENTIONS.width / 8 - CAMERA_ICON_DIMENTIONS.height / 2,
   right: PROFILE_IMAGE_DIMENTIONS.width / 8 - CAMERA_ICON_DIMENTIONS.width / 2,
 };
+const CAMERA_OPTIONS: CameraOptions = {
+  mediaType: 'photo',
+  includeBase64: false,
+  presentationStyle: 'fullScreen',
+  saveToPhotos: true,
+};
+const GALLERY_OPTIONS: ImageLibraryOptions = {
+  mediaType: 'photo',
+  includeBase64: false,
+  presentationStyle: 'fullScreen',
+  selectionLimit: 1,
+};
+
+type ImagePickType = 'capture' | 'library';
 
 export function ProfileImageUpload() {
-  const [profileImage] = useState(null); //TODO::remove this when integrations with the APIs && get profile related data for the store.
+  const [profileImageData, setProfileImageData] = useState<ImagePickerResponse>(null);
+
+  const loading = useSelector(state => state.loading.effects.userStore.changeProfilePicture);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (profileImageData?.errorMessage) {
+      ToastService.error({ message: 'Failed!', description: profileImageData?.errorMessage });
+    }
+  }, [profileImageData]);
+
+  const onButtonPress = async (type?: ImagePickType) => {
+    let result: ImagePickerResponse;
+
+    if (type === 'capture') {
+      result = await launchCamera(CAMERA_OPTIONS);
+    } else {
+      result = await launchImageLibrary(GALLERY_OPTIONS, setProfileImageData);
+    }
+
+    await dispatch.userStore.changeProfilePicture(result);
+    setProfileImageData(result);
+  };
 
   const renderProfileImage = useMemo(
     () =>
-      profileImage === null ? (
+      profileImageData === null || profileImageData?.assets?.length === 0 || loading ? (
         <View style={PROFILE_IMAGE_COMMON_STYLES}>
           {/* TODO::Create a helper function to get the first characters from full name when intergration with the APIs*/}
-          <Text variant={TextVariant.SubTitle1}>MW</Text>
+          {/* TODO::add a proper loading indicator */}
+          <Text variant={TextVariant.SubTitle1}>{loading ? 'loading' : 'MW'}</Text>
         </View>
       ) : (
         <FastImage
           style={PROFILE_IMAGE_COMMON_STYLES}
           source={{
-            uri: 'https://ymw.edu.in/wp-content/uploads/2022/02/dummy-profile-01.png', //TODO::remove this dummy URL when intergration with the APIs
+            uri: profileImageData.assets[0].uri,
           }}
         />
       ),
-    [profileImage],
+    [profileImageData, loading],
   );
 
   return (
     <View testID="ProfileImageComponentTestID" style={tw`m-auto`}>
       {renderProfileImage}
       <TouchableOpacity
-        style={tw`w-[${CAMERA_ICON_DIMENTIONS.width}px] h-[${CAMERA_ICON_DIMENTIONS.height}px] rounded-full absolute right-[${CAMERA_ICON_POSITION.right}px] top-[${CAMERA_ICON_POSITION.top}px]  justify-center items-center bg-Neutral-Sz100`}
-        onPress={() => NavigationService.openDrawer()}>
+        style={tw`w-[${CAMERA_ICON_DIMENTIONS.width}px] h-[${CAMERA_ICON_DIMENTIONS.height}px] rounded-full absolute right-[${CAMERA_ICON_POSITION.right}px] top-[${CAMERA_ICON_POSITION.top}px] justify-center items-center bg-Neutral-Sz100`}
+        onPress={() => {
+          //TODO::provide a proper choose UI to the end user
+          Alert.alert('Choose option', '', [
+            {
+              text: 'Camera',
+              onPress: () => {
+                onButtonPress('capture');
+              },
+            },
+            {
+              text: 'Gallery',
+              onPress: () => {
+                onButtonPress();
+              },
+            },
+          ]);
+        }}>
         <ProfileImageChangeCameraIcon />
       </TouchableOpacity>
     </View>
