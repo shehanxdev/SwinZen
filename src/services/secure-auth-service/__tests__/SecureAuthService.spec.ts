681ec572290eb33311b7616c5d23ae3e0cfb1dc0
Madusha Lakruwan <madusha@surge.global>
2023-05-21 19:31:58 +0530

    refactor: refactor getToken flow and update unit tests

--- src/services/secure-auth-service/__tests__/SecureAuthService.spec.ts
+++ src/services/secure-auth-service/__tests__/SecureAuthService.spec.ts
@@ -41,4 +41,112 @@ describe('SecureAuth Service', () => {
       service: TOKENS_KEYCHAIN_KEY,
     });
   });
+
+  describe('setItem', () => {
+    it('should call Keychain functions with correct arguments', async () => {
+      const key = 'testKey';
+      const value = { foo: 'bar' };
+
+      jest.spyOn(Keychain, 'setGenericPassword');
+      jest.spyOn(Keychain, 'resetGenericPassword');
+
+      // Disabling below eslint rule to test the private function in SecureAuthService
+      // eslint-disable-next-line @typescript-eslint/dot-notation
+      await SecureAuthService['setItem'](key, value);
+
+      // Verify the Keychain functions are called with correct arguments
+      expect(Keychain.resetGenericPassword).toHaveBeenCalledWith({ service: key });
+      expect(Keychain.setGenericPassword).toHaveBeenCalledWith(key, JSON.stringify(value), {
+        service: key,
+      });
+    });
+
+    it('should return the result from Keychain.setGenericPassword', async () => {
+      const key = 'testKey';
+      const value = { foo: 'bar' };
+      const expectedResult: Keychain.Result = {
+        service: '',
+        storage: '',
+      };
+
+      // Mock the setGenericPassword function to return the expected result
+      jest.spyOn(Keychain, 'setGenericPassword').mockResolvedValueOnce(expectedResult);
+
+      // Disabling below eslint rule to test the private function in SecureAuthService
+      // eslint-disable-next-line @typescript-eslint/dot-notation
+      const result = await SecureAuthService['setItem'](key, value);
+      expect(result).toStrictEqual(expectedResult);
+    });
+
+    it('should return false if an error occurs', async () => {
+      const key = 'testKey';
+      const value = { foo: 'bar' };
+
+      // Mock the setGenericPassword function to throw an error
+      jest.spyOn(Keychain, 'setGenericPassword').mockRejectedValueOnce(new Error('Mock error'));
+
+      // Disabling below eslint rule to test the private function in SecureAuthService
+      // eslint-disable-next-line @typescript-eslint/dot-notation
+      const result = await SecureAuthService['setItem'](key, value);
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('getItem', () => {
+    it('should call Keychain.getGenericPassword with the correct arguments', async () => {
+      const key = 'testKey';
+
+      jest.spyOn(Keychain, 'getGenericPassword');
+
+      // Disabling below eslint rule to test the private function in SecureAuthService
+      // eslint-disable-next-line @typescript-eslint/dot-notation
+      await SecureAuthService['getItem'](key);
+
+      // Verify that getGenericPassword is called with the correct arguments
+      expect(Keychain.getGenericPassword).toHaveBeenCalledWith({ service: key });
+    });
+
+    it('should return the parsed value if Keychain.getGenericPassword returns a result', async () => {
+      const key = 'testKey';
+      const password = JSON.stringify({ foo: 'bar' });
+      const expectedResult = { foo: 'bar' };
+
+      // Mock the getGenericPassword function to return a result
+      jest.spyOn(Keychain, 'getGenericPassword').mockResolvedValueOnce({
+        username: '',
+        password: password,
+        service: '',
+        storage: '',
+      });
+
+      // Disabling below eslint rule to test the private function in SecureAuthService
+      // eslint-disable-next-line @typescript-eslint/dot-notation
+      const result = await SecureAuthService['getItem'](key);
+      expect(result).toStrictEqual(expectedResult);
+    });
+
+    it('should return false if Keychain.getGenericPassword does not return a result', async () => {
+      const key = 'testKey';
+
+      // Mock the getGenericPassword function to return null
+      jest.spyOn(Keychain, 'getGenericPassword').mockResolvedValueOnce(null);
+
+      // Disabling below eslint rule to test the private function in SecureAuthService
+      // eslint-disable-next-line @typescript-eslint/dot-notation
+      const result = await SecureAuthService['getItem'](key);
+      expect(result).toBe(false);
+    });
+
+    it('should return false if an error occurs', async () => {
+      const key = 'testKey';
+
+      // Mock the getGenericPassword function to throw an error
+      jest.spyOn(Keychain, 'getGenericPassword').mockRejectedValueOnce(new Error('Mock error'));
+
+      // Disabling below eslint rule to test the private function in SecureAuthService
+      // eslint-disable-next-line @typescript-eslint/dot-notation
+      const result = await SecureAuthService['getItem'](key);
+      expect(result).toBe(false);
+    });
+  });
 });
