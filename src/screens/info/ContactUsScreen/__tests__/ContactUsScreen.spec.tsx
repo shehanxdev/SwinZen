55d8862c148dea1cc17876a69760a9047ef73594
Shehan Chanuka <shehan.chanuka@surge.global>
2023-05-26 14:17:18 +0530

    test: added snapshot tests for the contact us screen

--- src/screens/info/ContactUsScreen/__tests__/ContactUsScreen.spec.tsx
+++ src/screens/info/ContactUsScreen/__tests__/ContactUsScreen.spec.tsx
@@ -1,41 +1,23 @@
-// import * as React from 'react';
+import * as React from 'react';
 
-// import * as StoresModule from '@sz/stores';
-// import { renderWithProviders } from '@sz/utils';
+import { renderWithProviders } from '@sz/utils';
 
-// import { ContactUsScreen } from '../ContactUsScreen';
-
-// const MOCKED_ACCESS_TOCKEN =
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNoaWhhcmErODhAc3VyZ2UuZ2xvYmFsIiwic3ViIjoiYzMzMTA3NDAtZWU0OS00ZmM1LThjNjctZDA0YmJhZjBmMjc2IiwiaWF0IjoxNjg1MDA1MzgwLCJleHAiOjE2ODUwMDYyODB9.usldOS1WHDuPHkahfaSnKTAZHJwXVidxoTOnMI4wpNM';
+import { ContactUsScreen } from '../ContactUsScreen';
 
 describe('ContactUs Screen', () => {
-  //const testID = 'ContactUsScreenTestID';
-  //TODO:: remove following test. This test case is inteded only to pass git commit stage since test cases are still in development
-  it('should pass regardless', () => {
-    expect(true).toBe(true);
+  const testID = 'ContactUsScreenTestID';
+
+  const getRenderedScreen = () => renderWithProviders(<ContactUsScreen />);
+
+  it(`should render ContactUsScreen correctly`, () => {
+    const renderer = getRenderedScreen();
+    const renderTree = renderer.toJSON();
+    expect(renderTree).toMatchSnapshot();
   });
 
-  // const getRenderedScreen = () => renderWithProviders(<ContactUsScreen />);
-
-  // it(`should render ContactUsScreen correctly`, () => {
-  //   const useSelectorSpy = jest.spyOn(StoresModule, 'useSelector');
-  //   useSelectorSpy.mockImplementation(selector => {
-  //     if (selector.toString() === 'state => state.userStore.accessToken') {
-  //       console.log('Hello from mock');
-  //       return 'mocked-access-token';
-  //     }
-  //     // Return the actual implementation for other useSelector calls
-
-  //     return StoresModule.useSelector(selector);
-  //   });
-  //   const renderer = getRenderedScreen();
-  //   const renderTree = renderer.toJSON();
-  //   expect(renderTree).toMatchSnapshot();
-  // });
-
-  // it('should find the ContactUsScreen via testID', () => {
-  //   const { getByTestId } = getRenderedScreen();
-  //   const foundScreen = getByTestId(testID);
-  //   expect(foundScreen).toBeTruthy();
-  // });
+  it('should find the ContactUsScreen via testID', () => {
+    const { getByTestId } = getRenderedScreen();
+    const foundScreen = getByTestId(testID);
+    expect(foundScreen).toBeTruthy();
+  });
 });
