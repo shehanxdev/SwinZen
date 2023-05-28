ce1e5f880573e843f5cd01c2a118c01e6902ed1b
Manoj Udeshika <manojudeshika93@gmail.com>
2023-05-28 12:02:55 +0530

    feat: added timeline creation configs

---
+++ src/components/VideoPlayer/FFmpegWrapper.tsx
@@ -0,0 +1,39 @@
+import { FFmpegKit, FFmpegKitConfig, ReturnCode } from 'ffmpeg-kit-react-native';
+import RNFS from 'react-native-fs';
+
+const FRAME_PER_SEC = 1;
+const FRAME_WIDTH = 80;
+
+class FFmpegWrapper {
+  static getFrames(localFileName, videoURI, frameNumber, successCallback) {
+    let outputImagePath = `${RNFS.CachesDirectoryPath}/${localFileName}_%4d.png`;
+    const ffmpegCommand = `-ss 0 -i ${videoURI} -vf "fps=${FRAME_PER_SEC}/1:round=up,scale=${FRAME_WIDTH}:-2" -vframes ${frameNumber}`;
+
+    FFmpegKit.executeAsync(
+      ffmpegCommand,
+      async session => {
+        const state = FFmpegKitConfig.sessionStateToString(await session.getState());
+        const returnCode = await session.getReturnCode();
+        const failStackTrace = await session.getFailStackTrace();
+        const duration = await session.getDuration();
+
+        if (ReturnCode.isSuccess(returnCode)) {
+          console.log(`Encode completed successfully in ${duration} milliseconds;.`);
+          console.log(`Check at ${outputImagePath}`);
+          successCallback(outputImagePath);
+        } else {
+          console.log('Encode failed. Please check log for the details.');
+          console.log(`Encode failed with state ${state} and rc ${returnCode}.${failStackTrace}`);
+        }
+      },
+      log => {
+        console.log(log.getMessage());
+      },
+      statistics => {
+        console.log(statistics);
+      },
+    ).then(session => console.log(`Async FFmpeg process started with sessionId ${session.getSessionId()}.`));
+  }
+}
+
+export default FFmpegWrapper;
--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Pressable, View } from 'react-native';
+import { Image, Pressable, ScrollView, View } from 'react-native';
+import RNFS from 'react-native-fs';
 import Video from 'react-native-video';
 
 import { tw } from '@sz/config';
@@ -7,11 +8,18 @@ import { Color, TextVariant } from '@sz/constants';
 
 import { PauseIcon, PlayIcon } from '../Icon';
 import { Text } from '../Typography';
+import FFmpegWrapper from './FFmpegWrapper';
 
 export function VideoPlayer({ source }) {
   const [isPlaying, setIsPlaying] = useState(false);
   const [progress, setProgress] = useState('00:00');
   const [duration, setDuration] = useState('00:00');
+
+  const [newFrames, setNewFrames] = useState(null);
+  const FRAME_STATUS = Object.freeze({
+    LOADING: { name: Symbol('LOADING') },
+    READY: { name: Symbol('READY') },
+  });
 
   const convertToMinutesAndSeconds = seconds => {
     let minutes = Math.floor(seconds / 60);
@@ -28,8 +36,24 @@ export function VideoPlayer({ source }) {
   };
 
   const configureDuration = ({ duration }) => {
+    let localFileName = `${RNFS.CachesDirectoryPath}/${source}.mp4`;
+    const numberOfFrames = Math.ceil(duration);
     const durationInMinsAndSeconds = convertToMinutesAndSeconds(Math.round(duration));
     setDuration(durationInMinsAndSeconds);
+
+    setNewFrames(
+      Array(numberOfFrames).fill({
+        status: FRAME_STATUS.LOADING.name.description,
+      }),
+    );
+
+    FFmpegWrapper.getFrames(localFileName, source, numberOfFrames, filePath => {
+      let frames = [];
+      for (let i = 0; i < numberOfFrames; i++) {
+        frames.push(`${filePath.replace('%4d', String(i + 1).padStart(4, '0'))}.png`);
+      }
+      setNewFrames(frames);
+    });
   };
 
   return (
@@ -42,6 +66,28 @@ export function VideoPlayer({ source }) {
         paused={!isPlaying}
         style={tw`flex-1 w-full rounded-t-7.5 relative`}
       />
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        style={tw`w-screen`}
+        alwaysBounceHorizontal={true}
+        scrollEventThrottle={1}>
+        {newFrames?.map((frame, index) => {
+          if (frame.status === FRAME_STATUS.LOADING.name.description) {
+            return <View style={tw`w-10 h-10 border-2`} key={index}></View>;
+          } else {
+            return (
+              <Image
+                key={index}
+                source={{
+                  uri: 'file://' + frame,
+                }}
+                style={tw`w-10 h-10`}
+              />
+            );
+          }
+        })}
+      </ScrollView>
       <View style={tw`absolute bottom-2.5 flex-row justify-center w-full `}>
         <View style={tw`py-0.5 px-1.75 flex-row items-center w-23 bg-[#00000087] rounded-lg mx-0`}>
           <Pressable onPress={() => setIsPlaying(prevState => !prevState)}>
