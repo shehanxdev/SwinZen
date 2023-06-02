f1cb1fc156b9790531691675f9d781199c587f0b
Manoj Udeshika <manojudeshika93@gmail.com>
2023-06-02 14:43:59 +0530

    feat: added analysis screen content and testing

--- src/screens/main/AnalysisScreen/AnalysisScreen.tsx
+++ src/screens/main/AnalysisScreen/AnalysisScreen.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { View } from 'react-native';
 
-import { ScoreType } from '@sz/constants';
+import { Button } from '@sz/components';
+import { tw } from '@sz/config';
+import { Color, Route, ScoreType } from '@sz/constants';
+import { NavigationService } from '@sz/services';
 
-import { BaseMainScreen } from '../components';
+import { BaseMainScreen, MonthSelector } from '../components';
 import { AnalysisDataCard } from './components';
 
 export function AnalysisScreen() {
   return (
-    <BaseMainScreen>
-      <AnalysisDataCard score={15} observation={ScoreType.OVERALL} time={new Date()} />
+    <BaseMainScreen testID="AnalysisScreenTestID">
+      <View style={tw`flex-1 justify-between`}>
+        <View style={tw`mt-13 pb-3.25 rounded-2.5 bg-Transparency-dark`}>
+          <View style={tw`pt-1 pb-5`}>
+            <MonthSelector textColor={Color.Neutral.Sz200} />
+          </View>
+          <AnalysisDataCard score={15} observation={ScoreType.OVERALL} time={new Date()} />
+          <AnalysisDataCard score={8} observation={ScoreType.SETUP} time={new Date()} />
+          <AnalysisDataCard score={3} observation={ScoreType.BACKSWING} time={new Date()} />
+          <AnalysisDataCard score={12} observation={ScoreType.DOWNSWING} time={new Date()} />
+        </View>
+      </View>
+      <View style={tw`mb-6`}>
+        <Button onPress={() => NavigationService.navigate(Route.VideosTab)} title={'view all my videos'} />
+      </View>
     </BaseMainScreen>
   );
 }
--- src/screens/main/AnalysisScreen/components/AnalysisDataCard/AnalysisDataCard.tsx
+++ src/screens/main/AnalysisScreen/components/AnalysisDataCard/AnalysisDataCard.tsx
@@ -22,7 +22,7 @@ export function AnalysisDataCard({
   return (
     <View
       testID={testID}
-      style={tw`flex-row h-16 mx-2 px-5 bg-Neutral-Sz1000 rounded-2.5 items-center justify-between`}>
+      style={tw`flex-row h-16 mx-2 my-1.75 px-5 bg-Neutral-Sz1000 rounded-2.5 items-center justify-between`}>
       <View style={tw`flex-row items-center gap-7.5`}>
         {/* design shadow and this implemntation has a different, here can't use arbitary values to shadow, added closest tailwind style */}
         <View
