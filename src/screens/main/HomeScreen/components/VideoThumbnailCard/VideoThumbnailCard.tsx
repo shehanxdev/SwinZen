03be489496507430108af96dcce3e6058d39ba21
Shehan Chanuka <shehan.chanuka@surge.global>
2023-06-02 09:48:25 +0530

    refactor: refactored video thumbnail card

--- src/screens/main/HomeScreen/components/VideoThumbnailCard/VideoThumbnailCard.tsx
+++ src/screens/main/HomeScreen/components/VideoThumbnailCard/VideoThumbnailCard.tsx
@@ -8,13 +8,15 @@ import { VideoUploadCardFooter } from '../VideoUploadCard';
 
 export function VideoThumbnailCard() {
   return (
-    <View style={tw`h-54.5 rounded-2.5 overflow-hidden`}>
-      <ImageBackground
-        source={{ uri: 'https://i.ibb.co/XFvHx8J/Rectangle-132.png' }}
-        resizeMode="cover"
-        style={tw`flex-1 items-center justify-center`}>
-        <PlayButtonIcon />
-      </ImageBackground>
+    <View>
+      <View style={tw`h-54.5 rounded-2.5 rounded-bl-[20px] overflow-hidden`}>
+        <ImageBackground
+          source={{ uri: 'https://i.ibb.co/XFvHx8J/Rectangle-132.png' }}
+          resizeMode="cover"
+          style={tw`flex-1 items-center justify-center`}>
+          <PlayButtonIcon />
+        </ImageBackground>
+      </View>
       <VideoUploadCardFooter date="04 JUN 2022" cameraAngle="Faceview" results="10 • Pass" />
     </View>
   );
