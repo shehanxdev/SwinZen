f01580c6006d2849ee3a33005f50d2ee9acea6fa
Shehan Chanuka <shehan.chanuka@surge.global>
2023-06-06 21:59:27 +0530

    test: added snapshot tests for VideoThumbnailCard

---
+++ src/screens/main/HomeScreen/components/VideoThumbnailCard/__tests__/VideoThumbnailCard.spec.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+import { renderWithProviders } from '@sz/test-utils';
+
+import { VideoThumbnailCard } from '../VideoThumbnailCard';
+
+describe('Home screen', () => {
+  const mockVideo = {
+    id: 'id4',
+    userId: 'string',
+    name: 'string',
+    videoUrl: 'string',
+    videoType: 'Face View',
+    thumbnailUrl: 'https://i.ibb.co/XFvHx8J/Rectangle-132.png',
+    grading: 4.7,
+    createdAt: '2023-06-04T13:22:29.181Z',
+  };
+  const getRenderedScreen = () => renderWithProviders(<VideoThumbnailCard video={mockVideo} />);
+
+  it(`should render Home screen correctly`, async () => {
+    const renderer = getRenderedScreen();
+    const renderTree = renderer.toJSON();
+    expect(renderTree).toMatchSnapshot();
+  });
+});
