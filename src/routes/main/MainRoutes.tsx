b9ee73d4dd7c2aa7c347367901c9daae3875ce5a
Manoj Udeshika <manojudeshika93@gmail.com>
2023-06-08 17:40:36 +0530

    chore: renamed files

--- src/routes/main/MainRoutes.tsx
+++ src/routes/main/MainRoutes.tsx
@@ -7,7 +7,7 @@ import { useFirebaseNotifications } from '@sz/hooks';
 import { AccountStack } from '../account';
 import { LibraryStack } from '../library';
 import { PricePlansStack } from '../pricePlans';
-import { VideoUploadStack } from '../videoUpload';
+import { VideoUploadStack } from '../videosUpload';
 import { MainDrawerStack } from './MainDrawerRoutes';
 
 const Stack = createNativeStackNavigator();
