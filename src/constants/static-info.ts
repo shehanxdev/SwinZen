6dd132e6d04bc97c76351c75b87d2a196dcfb4bf
Madusha Lakruwan <madusha@surge.global>
2023-06-15 11:53:57 +0530

    fix: update image urls

--- src/constants/static-info.ts
+++ src/constants/static-info.ts
@@ -1,14 +1,14 @@
 export const SwingZenUrl = 'https://swingzen.com';
 export const SwingZenMail = 'info@swingzen.com';
 
-export const DownTheLineGIF = 'https://thumbnails.dev.swingzen.com/gifs/downtheline.gif';
-export const FaceOnGIF = 'https://thumbnails.dev.swingzen.com/gifs/faceon.gif';
-export const LandscapeGIF = 'https://thumbnails.dev.swingzen.com/gifs/Landscape.gif';
-export const TripodHeightGIF = 'https://thumbnails.dev.swingzen.com/gifs/tripodheight.gif';
+export const DownTheLineGIF = 'https://files.dev.swingzen.com/gifs/downtheline.gif';
+export const FaceOnGIF = 'https://files.dev.swingzen.com/gifs/faceon.gif';
+export const LandscapeGIF = 'https://files.dev.swingzen.com/gifs/Landscape.gif';
+export const TripodHeightGIF = 'https://files.dev.swingzen.com/gifs/tripodheight.gif';
 
-export const LandscapeImageUrl = 'https://thumbnails.dev.swingzen.com/video-thumbs/landscape.png';
-export const DownTheLineImageUrl = 'https://thumbnails.dev.swingzen.com/video-thumbs/downtheline.png';
-export const FaceOnImageUrl = 'https://thumbnails.dev.swingzen.com/video-thumbs/faceon.png';
+export const LandscapeImageUrl = 'https://files.dev.swingzen.com/video-thumbs/landscape.png';
+export const DownTheLineImageUrl = 'https://files.dev.swingzen.com/video-thumbs/downtheline.png';
+export const FaceOnImageUrl = 'https://files.dev.swingzen.com/video-thumbs/faceon.png';
 
 // static measurements
 export const headerTopMargin = 20; // this margin is only applicable into android when we are using custom header
