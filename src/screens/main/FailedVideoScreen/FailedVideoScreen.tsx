3d4dfb9093b0480790dc2e9bb2f7c8d43094dcfa
Shehan Chanuka <shehan.chanuka@surge.global>
2023-06-15 14:34:31 +0530

    feat: completed VideoFailed Screen UI

--- src/screens/main/FailedVideoScreen/FailedVideoScreen.tsx
+++ src/screens/main/FailedVideoScreen/FailedVideoScreen.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import { View } from 'react-native';
+
+import { Text } from '@sz/components';
+import { tw } from '@sz/config';
+import { TextAlignment, TextVariant } from '@sz/constants';
 
 import { BaseScreen } from '../../components/BaseScreen';
 import { VideoUploadErrorCard } from '../components';
+import { dummyvideoUploadFailedReasons as videoAnalysisFailedReasons } from './dummyvideoUploadFailedReasons';
 
 export function FailedVideoScreen() {
   return (
     <BaseScreen>
-      <VideoUploadErrorCard isLinkAvailable={false} hasFooter={false} />
+      <View style={tw`mx-4 mt-5`}>
+        <VideoUploadErrorCard showFooterAndTextLink={false} />
+        <View style={tw`mt-8`}>
+          <Text variant={TextVariant.Body1Regular} textAlign={TextAlignment.Left}>
+            Here's why your video has failed
+          </Text>
+          <View style={tw`ml-9 mr-10.5 mt-2.5`}>
+            {videoAnalysisFailedReasons.map((reason, key) => {
+              return (
+                <Text
+                  key={key}
+                  variant={TextVariant.Body2Regular}
+                  textAlign={TextAlignment.Left}>{`\u2022 ${reason}`}</Text>
+              );
+            })}
+          </View>
+        </View>
+      </View>
     </BaseScreen>
   );
 }
---
+++ src/screens/main/FailedVideoScreen/dummyvideoUploadFailedReasons.ts
@@ -0,0 +1,7 @@
+export const dummyvideoUploadFailedReasons = [
+  'Too far away from golfer',
+  'Too close to golfer',
+  'Poor lighting. (Usually when indoors or at dusk outside.)',
+  'Too much clutter in background (Usually indoors)',
+  'More than one person in video',
+];
