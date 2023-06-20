23fd0bd745edd1648925b69f6794444ac0067e1e
shihara <shihara@surge.global>
2023-06-20 11:29:22 +0530

    fix: OTP timer value reduced to 1

--- src/constants/auth/otp-data.enum.ts
+++ src/constants/auth/otp-data.enum.ts
@@ -3,5 +3,5 @@ export enum OtpType {
   FORGOT_PASSWORD = 'FORGOT_PASSWORD',
 }
 
-export const DEFAULT_OTP_RESEND_TIMER_MINUTES = 5; //TODO::as for the requirements this values should be 1 minute. But the current BE has configured to have 5 minutes.
+export const DEFAULT_OTP_RESEND_TIMER_MINUTES = 1;
 export const DEFAULT_OTP_RESEND_TIMER_SECONDS = 0;
